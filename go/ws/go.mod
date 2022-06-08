87f8fe74d2695e781d1fec223d42431b82e2c3fa
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-06-08 23:08:32 +0800

    add ws endpoint

--- go/ws/go.mod
+++ go/ws/go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
 )
 
-require github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+require (
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
--- go/ws/internal/handlers/handlers.go
+++ go/ws/internal/handlers/handlers.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
-	"github.com/CloudyKit/jet/v6"
 	"log"
 	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -11,9 +13,40 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 // Home renders the home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// WsJsonResponse defines the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+// WsEndpont upgrades connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 	}
