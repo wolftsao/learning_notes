7b0abba43c87bf993c7e20a9f51f6ddfad4e02ab
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-06-14 09:15:58 +0800

    add ws client in html

---
+++ go/mygoblueprints/sprinkle/go.mod
@@ -0,0 +1,3 @@
+module github.com/wolftsao/learning_notes/go/mygoblueprints/sprinkle
+
+go 1.18
--- go/mygoblueprints/sprinkle/main.go
+++ go/mygoblueprints/sprinkle/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
+const otherWord = "*"
+
+var transforms = []string{
+	otherWord,
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets " + otherWord,
+	otherWord + "hq",
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixMicro())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		t := transforms[rand.Intn(len(transforms))]
+		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	}
 }
