4189c776ff9120b7c2f2eb625766ca27a93e69cf
nathan_tsao <nathan_tsao@paradise-soft.com.tw>
2022-07-15 09:17:51 +0800

    add grpc practice

---
+++ grpc/grpc_up_and_running/productinfo/service/productinfo_service.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	pb "productinfo/service/ecommerce"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type server struct {
+	productMap map[string]*pb.Product
+	pb.UnimplementedProductInfoServer
+}
+
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out := uuid.New()
+
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+
+	s.productMap[in.Id] = in
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+
+	return nil, status.Errorf(codes.NotFound, "Product does not exist: %s", in.Value)
+}
